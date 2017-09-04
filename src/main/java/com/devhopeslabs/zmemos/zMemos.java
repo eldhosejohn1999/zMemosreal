4ae67368c769074538d23741f00fe552c00d0144
Eldhose John <eldhosejohn1999@gmail.com>
2017-09-04 23:43:45 +0530

    main commit

--- src/main/java/com/devhopeslabs/zmemos/zMemos.java
+++ src/main/java/com/devhopeslabs/zmemos/zMemos.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class zMemos {
+public class zMemos {//main app
     public static void main(String[] args) {
         SpringApplication.run(zMemos.class,args);
     }
-} 
+}
